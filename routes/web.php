9f8ad3ce65df3118b4f1978f4074a0362610c699
reza-32 <reza.rza@bsi.ac.id>
2024-10-17 15:55:26 +0700

    Commit Pertama

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LoginController;
+
+Route::get('/', function () {
+    //return view('welcome');
+    return redirect()->route('backend.login');
+});
+Route::get('backend/beranda',[BerandaController::class, 'berandaBackend'])
+->name('backend.beranda')->middleware('auth');
+
+Route::get('backend/login',[LoginController::class, 'loginBackend'])
+->name('backend.login');
+Route::post('backend/login', [LoginController::class, 'authenticateBackend'])
+->name('backend.login');
+Route::post('backend/logout', [LoginController::class, 'logoutBackend'])
+->name('backend.logout');
